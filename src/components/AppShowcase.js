c9e32f7b23a99ea37b0e1afe8958459d915f5444
jorlar <jqern.larsen@gmail.com>
2025-09-25 22:45:33 +0200

    :bug: Fix text

--- src/components/AppShowcase.js
+++ src/components/AppShowcase.js
@@ -37,7 +37,7 @@ const AppShowcase = () => {
   return (
     <section id="app-showcase" className="section app-showcase">
       <div className="container">
-        <h2 className="section-title">Slk fungerer Fritim</h2>
+        <h2 className="section-title">Slik fungerer Fritim</h2>
         <p className="section-subtitle">
           Fritim er en plattform som frigjør tid for familier, skaper muligheter for ungdom og bygger fellesskap i idrett og organisasjoner. Vi kobler sammen klubber, foreldre, frivillige og støttespillere i én løsning
         </p>
